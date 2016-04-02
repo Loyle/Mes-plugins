2ae34fd7cb21d72e3cb72091e869a6e9d22bfe35
Loyle <c.antoine81@laposte.net>
2016-04-02 20:34:33 +0200

    Repo update

--- CutTheSaplings/src/com/gmail/LoyleCraft/CutTheSaplings/Commands/BasicCommand.java
+++
@@ -1,9 +0,0 @@
-package com.gmail.LoyleCraft.CutTheSaplings.Commands;
-
-import org.bukkit.entity.Player;
-
-public abstract interface BasicCommand {
-	public abstract boolean onCommand(Player paramPlayer, String[] paramArrayOfString);
-	public abstract String help(Player paramPlayer);
-	public abstract String getPermission();
-}
